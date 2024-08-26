3e655241c2a1c619822fec55162608609d9a85b3
Vee Pabuence Vargas <beepabuence@gmail.com>
2024-08-25 20:44:05 -0500

    Replace function created

---
+++ src/JavaScript/index.js
@@ -0,0 +1,22 @@
+function encryptor() {
+    let textArea = document.querySelector('#inputText').value;
+    let areaEncrytor = document.querySelector('#containerEncryptor');
+    
+    let conversiones = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+
+    let result = textArea.split('').map(char => conversiones[char] || char).join('');
+    while (areaEncrytor.firstChild) {
+        areaEncrytor.removeChild(areaEncrytor.firstChild)
+    }
+
+    areaEncrytor.id = 'containerWithEncryptor';
+    areaEncrytor.innerHTML = 
+        `<p class="textEncryptor">${result}</p>
+        <button class="button" id="buttonCopy">Copiar</button>`
+}
